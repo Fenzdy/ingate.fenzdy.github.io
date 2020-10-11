35a335b834380dcddd8f28979c5424cf2a163408
Max <39224167+Fenzdy@users.noreply.github.com>
2020-10-11 15:20:45 +0300

    v.1.2
    Валидация формы, ajax отправка

--- script/main.js
+++ script/main.js
@@ -1,16 +1,3 @@
-
-
-
-// всплывашка информация про спикера
-let speakInfo = document.querySelector('.speak-info');
-let spealInfoModal = document.querySelector('.about__speaker-info');
-
-speakInfo.addEventListener('click', () => {
- console.log('test');
- spealInfoModal.classList.toggle('hide');
-});
-
-
 // плавный скрол к форме 
 let buttonHeader = document.querySelector('.button');
 let buttonAbout = document.querySelector('.about__button');
@@ -32,17 +19,16 @@ let iti = window.intlTelInput(phoneInput, {
   initialCountry: "ru",
 })
 
+
 // вкладки описания уроков
+let speakInfo = document.querySelectorAll('.speak-info');
+let spealInfoModal = document.querySelectorAll('.about__speaker-info');
 let tabs = document.querySelectorAll('.tab-1');
 let contentTab = document.querySelectorAll('.content-tab');
-console.log(tabs);
-console.log(contentTab);
 
 for(let i = 0; i < tabs.length; i++) {
-  
   tabs[i].addEventListener('click', (event) => {
     event.preventDefault();
-    console.log(tabs[i]);
     // меняем классы
     for(let j = 0; j < contentTab.length; j++) {
       if(j == i) {
@@ -56,4 +42,59 @@ for(let i = 0; i < tabs.length; i++) {
     }
     
   });
+  // всплывашка информация про спикера
+  speakInfo[i].addEventListener('click', (e) => {
+    spealInfoModal[i].classList.toggle('hide');
+  });
 }
+
+
+// валидация и отправка формы
+$('#formGo').on('click', () => {
+  // валидация
+  let wrapperForm = document.querySelectorAll('.chek-valid-wrap');
+  let noValidForm = document.querySelectorAll('.no-valid-form');
+  let inpForm = document.querySelectorAll('.chek-valid');
+
+  for(let i = 0; i < wrapperForm.length; i++) {
+    if(!inpForm[i].value) {
+      
+      inpForm[i].classList.add('no-valid-inp');
+      wrapperForm[i].classList.add('request__inp-item--wrapper-valid');
+      noValidForm[i].classList.remove('hide');
+      return false;
+    } else {
+      inpForm[i].classList.remove('no-valid-inp');
+      wrapperForm[i].classList.remove('request__inp-item--wrapper-valid');
+      noValidForm[i].classList.add('hide');
+    }
+  }
+  let email = $('#email').val().trim();
+  let name = $('#name').val().trim();
+  let phone = $('#phone').val().trim();
+  let promo = $('#promo').val().trim();
+
+  // отправка формы
+  $.ajax ({
+    url: 'script/mail.php',
+    type: 'POST',
+    cache: false,
+    data: {
+      'name': name, 
+      'email': email, 
+      'phone': phone, 
+      'promo': promo,
+    },
+    dataType: 'html',
+    beforeSend: function () {
+    },
+    success: function(data) {
+      alert(data);
+    }
+    
+  });
+  return false;
+});
+
+
+
